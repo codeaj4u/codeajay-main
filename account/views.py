9263c66410e1bd0e48b722b8b7144dca868b466c
ajayoneness <ajayoneness123@gmail.com>
2023-12-23 19:20:23 +0545

    🤪🤪🤪🤪🤪🤪🤪🤪🤪

--- account/views.py
+++ account/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
 from projects.models import Project
 from api.models import post_create,blog_category
+import asyncio
+from telegram import Bot
+
+
+async def send_telegram_message(message,image_path="null",teligram_token='6365955881:AAH-BYuMTer0UzmIJNbY04kI4awKHrm7maU',chat_id='6747562361'):
+    try:
+        bot = Bot(token=teligram_token)
+        chat_id =chat_id
+        message_text = message
+        await bot.send_message(chat_id=chat_id, text=message_text)
+
+        if image_path == "null":
+            pass
+        else:
+            with open(image_path, 'rb') as photo:
+                await bot.send_photo(chat_id=chat_id, photo=photo)
+    except:
+        print("message is not sent")
+
+
 
 def home(request):
+
+    if request.method == "POST":
+        name = request.POST['name']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        mes = f'''
+codeAjay.in
+        
+Name : {name}
+Contact : {contact}
+Email : {email}
+Message : {message}
+'''
+
+
+        print(mes)
+        asyncio.run(send_telegram_message(mes))
+        print("send !!")
+
+
     all_blog = post_create.objects.all().order_by('-id')
     three_blog = post_create.objects.all().order_by('-id')[:3]
     cat = blog_category.objects.all()
@@ -12,16 +54,77 @@ def home(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        mes = f'''
+    codeAjay.in
+
+    Name : {name}
+    Contact : {contact}
+    Email : {email}
+    Message : {message}
+    '''
+
+        print(mes)
+        asyncio.run(send_telegram_message(mes))
+        print("send !!")
+
+
+
     cat = blog_category.objects.all()
     all_blog = post_create.objects.all().order_by('-id')
     return render(request, 'contact.html',{"blogs":all_blog,'cat':cat})
 
 def about(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        mes = f'''
+    codeAjay.in
+
+    Name : {name}
+    Contact : {contact}
+    Email : {email}
+    Message : {message}
+    '''
+
+        print(mes)
+        asyncio.run(send_telegram_message(mes))
+        print("send !!")
+
+
     cat = blog_category.objects.all()
     all_blog = post_create.objects.all().order_by('-id')
     return render(request, 'about.html' ,{"blogs":all_blog,'cat':cat})
 
 def privacy_policy(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        mes = f'''
+    codeAjay.in
+
+    Name : {name}
+    Contact : {contact}
+    Email : {email}
+    Message : {message}
+    '''
+
+        print(mes)
+        asyncio.run(send_telegram_message(mes))
+        print("send !!")
+
+
     cat = blog_category.objects.all()
     all_blog = post_create.objects.all().order_by('-id')
     return render(request, 'privacy-policy.html',{"blogs":all_blog,'cat':cat})
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from api.models import post_create,blog_category
+import asyncio
+from account.views import send_telegram_message
+
 
 
 def singleblog(request,slug):
+
+
+    if request.method == "POST":
+        name = request.POST['name']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        mes = f'''
+    codeAjay.in
+
+    Name : {name}
+    Contact : {contact}
+    Email : {email}
+    Message : {message}
+    '''
+
+        print(mes)
+        asyncio.run(send_telegram_message(mes))
+        print("send !!")
+
+
     top_blog = post_create.objects.get(slug=slug)
     all_blog = post_create.objects.all().order_by('-id')
     cat = blog_category.objects.all()
@@ -11,6 +36,25 @@ def singleblog(request,slug):
 
 
 def category(request,cat):
+    if request.method == "POST":
+        name = request.POST['name']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        mes = f'''
+    codeAjay.in
+
+    Name : {name}
+    Contact : {contact}
+    Email : {email}
+    Message : {message}
+    '''
+
+        print(mes)
+        asyncio.run(send_telegram_message(mes))
+        print("send !!")
+
     filtered_posts = post_create.objects.filter(category__slug=cat).order_by('-id')
     cat = blog_category.objects.all()
     all_blog = post_create.objects.all().order_by('-id')
